193f08f40f693c6b2b52f523723cfda6ffab260e
Kuzma Shevelev <seffele@gmail.com>
2023-11-25 22:49:26 +0300

    chage styles

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const code = (new URL(window.location.href)).searchParams.get('code') || "no code detected";
 const codeSpan = document.querySelector('#code');
-
-codeSpan.innerText = "Code: " + code ;
+codeSpan.innerText = code ;
